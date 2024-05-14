b12480c2a30ea0a992712a2a5b51e7ecb3fbedb1
Mark Oliver Mints <mmints@uni-koblenz.de>
2024-05-14 17:48:43 +0200

    [Add] Track cleaning process (WIP: Write out fails)

--- vacusim_robot_driver/controllers/large_ground/large_ground.c
+++ vacusim_robot_driver/controllers/large_ground/large_ground.c
@@ -23,7 +23,10 @@
  * Modified by Mark O. Mints (mmints@uni-koblenz.de)
  */
 
+
 #include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
 #include <webots/display.h>
 #include <webots/robot.h>
 #include <webots/supervisor.h>
@@ -38,8 +41,58 @@
 #define GROUND_X 14.9
 #define GROUND_Y 9.9
 
+// Evaluation
+#define EVALUATION_TIME 60.0 // Ten minutes
+
+#define MAP_WIDTH 768
+#define MAP_HEIGHT 512
+
+void fillMap(int map[MAP_HEIGHT][MAP_WIDTH], const int i, const int j, const float radius)
+{
+    for (int x = 0; x < MAP_WIDTH; x++) {
+        for (int y = 0; y < MAP_HEIGHT; y++) {
+            int dx = x - j;
+            int dy = y - i;
+            double distance = sqrt(dx * dx + dy * dy);
+
+            if (distance <= radius) {
+                map[y][x] = 255;
+            }
+        }
+    }
+}
+
+int countCleanElements(int map[MAP_HEIGHT][MAP_WIDTH]) {
+    int count = 0;
+
+    for (int i = 0; i < MAP_HEIGHT; i++) {
+        for (int j = 0; j < MAP_WIDTH; j++) {
+            if (map[i][j] == 255) {
+                count++;
+            }
+        }
+    }
+
+    return count;
+}
+
 // main function
 int main() {
+
+    FILE *file;
+       
+    // Open the file in write mode
+    file = fopen("result.csv", "w");
+    
+    if (file == NULL) {
+        printf("Error opening the file.\n");
+        return 1;
+    }
+    
+    // Header
+    fprintf(file, "time;cleaning_progress\n");
+
+    
   // init Webtos stuff
   wb_robot_init();
 
@@ -47,11 +100,34 @@ int main() {
   WbDeviceTag display = wb_robot_get_device("large_ground_display");
 
   // get the properties of the Display
-  int width = wb_display_get_width(display);
-  int height = wb_display_get_height(display);
+  int width = wb_display_get_width(display);    // 768
+  int height = wb_display_get_height(display);  // 512
 
-  // prepare stuff to get the
-  // Robot(IROBOT_CREATE).translation field
+
+      FILE *map_file;
+    map_file = fopen("result_map.pgm", "w");
+    fprintf(map_file, "P2\n768 512\n255\n");
+
+    // Map for logging the driven route and to calculate the cleaned area
+    // col, row 
+    int map[width][height];
+    int dirty_elements = 0;
+    int cleaned_elements = 0;
+
+    // Initialize Map
+    // TODO: Add Walls
+    for (int i = 0; i < width; i++) {
+        for (int j = 0; j < height; j++) {
+            // if ((i <= 14) || (j <= 14) || (i >= 498)|| (j >= 498) || (i >= 162 && i <= 195 && j <= 232)) {
+            //     map[j][i] = 0;
+            // } else {
+                map[j][i] = 128;
+                dirty_elements++;
+            // }
+        }
+    }
+
+  // prepare stuff to get the Robot(IROBOT_CREATE).translation field
   WbNodeRef mybot = wb_supervisor_node_get_from_def("CUSTOM_ROOMBA");
   WbFieldRef translationField = wb_supervisor_node_get_field(mybot, "translation");
 
@@ -62,14 +138,62 @@ int main() {
   // set the pen to remove the texture
   wb_display_set_alpha(display, 0.0);
 
+  // For timing
+  int last_casted_time = 0;
+  float cleaning_progress = 0.0;
+    
   while (wb_robot_step(TIME_STEP) != -1) {
+
     // Update the translation field
     const double *translation = wb_supervisor_field_get_sf_vec3f(translationField);
+    const double transform_x = width * (translation[X] + GROUND_X / 2) / GROUND_X;
+    const double transform_y = height * (-translation[Y] + GROUND_Y / 2) / GROUND_Y;
+  
+    // printf("Current Robot Position: (%i,%i)\n", (int)round(transform_x), (int)round(transform_y));
 
     // display the robot position
-    wb_display_fill_oval(display, width * (translation[X] + GROUND_X / 2) / GROUND_X,
-                         height * (-translation[Y] + GROUND_Y / 2) / GROUND_Y, 7, 7);
+    wb_display_fill_oval(display, transform_x, transform_y, 7, 7);
+
+    // Robot Coordinates
+    // TODO: Scaling with map dimensions?
+    // TODO: Move this into the robot controller
+    // const double coord_x = 60 * (translation[X] + GROUND_X / 2) / GROUND_X;  // Factor in x dimension
+    // const double coord_y = 40 * (-translation[Y] + GROUND_Y / 2) / GROUND_Y; // Factor in y dimension
+    // printf("Current Robot Position: (%i,%i)\n", (int)round(coord_x), (int)round(coord_y));
+    // printf("Current Robot Position: (%f,%f)\n", coord_x, coord_y);
+    
+   fillMap(map, (int)round(transform_y), (int)round(transform_x), 7.0);
+    cleaned_elements = countCleanElements(map);
+    cleaning_progress = (float)cleaned_elements / (float)dirty_elements;
+  
+    const double simulation_time = wb_robot_get_time();
+        
+    if (((int)simulation_time % 5 == 0) && ((int)simulation_time != last_casted_time)) {
+      last_casted_time = (int)simulation_time;
+      printf("Current Time: %i Seconds - Cleaning Progress: %f%%\n", last_casted_time, (cleaning_progress * 100.0));
+      fprintf(file, "%i;%f\n", last_casted_time, cleaning_progress);
+    }
+    
+    if (simulation_time >= EVALUATION_TIME) {
+      printf("Timeout!\n");
+      break;
+    }
   }
+  printf("Experiment is done!\n");
+  
+    // Printing the map for verification
+    for (int i = 0; i < 512; i++) {
+        for (int j = 0; j < 512; j++) {
+            fprintf(map_file, "%d ", map[i][j]); // MAP [768][512]
+        }
+        fprintf(map_file, "\n");
+    }
+
+    fclose(map_file);
+  
+  // Close the file
+  fclose(file);
+  printf("File created successfully.\n");
 
   wb_robot_cleanup();
 
