567db15fbed6f4dc6874179f34970cfc9c930490
Mark Oliver Mints <mark@mints.de>
2024-05-14 10:55:36 +0200

    [Update] Save worlds in uniform format

--- vacusim_robot_driver/controllers/ground/build/release/ground.d
+++ vacusim_robot_driver/controllers/ground/build/release/ground.d
@@ -1,7 +1,7 @@
 build/release/ground.o: ground.c \
- /home/ros-user/.ros/webotsR2023a/webots/include/controller/c/webots/display.h \
- /home/ros-user/.ros/webotsR2023a/webots/include/controller/c/webots/types.h \
- /home/ros-user/.ros/webotsR2023a/webots/include/controller/c/webots/robot.h \
- /home/ros-user/.ros/webotsR2023a/webots/include/controller/c/webots/nodes.h \
- /home/ros-user/.ros/webotsR2023a/webots/include/controller/c/webots/supervisor.h \
- /home/ros-user/.ros/webotsR2023a/webots/include/controller/c/webots/contact_point.h
+  /Applications/Webots.app/Contents/include/controller/c/webots/display.h \
+  /Applications/Webots.app/Contents/include/controller/c/webots/types.h \
+  /Applications/Webots.app/Contents/include/controller/c/webots/robot.h \
+  /Applications/Webots.app/Contents/include/controller/c/webots/nodes.h \
+  /Applications/Webots.app/Contents/include/controller/c/webots/supervisor.h \
+  /Applications/Webots.app/Contents/include/controller/c/webots/contact_point.h
