925693fab983ff60f0a1c7389c7a67aeab588991
Mark Oliver Mints <mmints@uni-koblenz.de>
2024-05-14 15:20:02 +0200

    [Add] Controller for large apartment

---
+++ vacusim_robot_driver/controllers/large_ground/build/release/large_ground.d
@@ -0,0 +1,7 @@
+build/release/large_ground.o: large_ground.c \
+ /home/ecvb/.ros/webotsR2023b/webots/include/controller/c/webots/display.h \
+ /home/ecvb/.ros/webotsR2023b/webots/include/controller/c/webots/types.h \
+ /home/ecvb/.ros/webotsR2023b/webots/include/controller/c/webots/robot.h \
+ /home/ecvb/.ros/webotsR2023b/webots/include/controller/c/webots/nodes.h \
+ /home/ecvb/.ros/webotsR2023b/webots/include/controller/c/webots/supervisor.h \
+ /home/ecvb/.ros/webotsR2023b/webots/include/controller/c/webots/contact_point.h
