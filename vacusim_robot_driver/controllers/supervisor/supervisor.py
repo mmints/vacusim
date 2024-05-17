d4f786645dc6ae44e8068e414ab498567bb03a94
Mark Oliver Mints <mmints@uni-koblenz.de>
2024-05-17 07:23:45 +0200

    [Update] Send pose via udp

--- vacusim_robot_driver/controllers/supervisor/supervisor.py
+++ vacusim_robot_driver/controllers/supervisor/supervisor.py
@@ -1,10 +1,31 @@
+'''
+Author: Mark O. Mints (mmints@uni-koblenz.de)
+'''
+
 import sys
 import numpy as np
+
+import socket
+import struct
 
 from controller import Supervisor
 
 
 TIME_STEP = 32
+
+
+def send_floats_via_udp(ip_address, port, floats):
+    # Create a UDP socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    # Pack the floats into a binary format
+    data = struct.pack('!fff', *floats)
+
+    # Send the data
+    sock.sendto(data, (ip_address, port))
+
+    # Close the socket
+    sock.close()
 
 supervisor = Supervisor()
 
@@ -30,13 +51,16 @@ while supervisor.step(TIME_STEP) != -1:
     translation = trans_field.getSFVec3f()
     rotation = rot_field.getSFRotation()
 
-    tile_cord_x = int(dim[0] * (translation[0] + (dim[0]*dim[2]) / 2) / (dim[0]*dim[2]))
-    tile_cord_y = int(dim[1] * (- translation[1] + (dim[1]*dim[2]) / 2) / (dim[1]*dim[2]))
+    tile_cord_x = dim[0] * (translation[0] + (dim[0]*dim[2]) / 2) / (dim[0]*dim[2])
+    tile_cord_y = dim[1] * (- translation[1] + (dim[1]*dim[2]) / 2) / (dim[1]*dim[2])
     tile_pose = np.array([tile_cord_x, tile_cord_y, rotation[3]])
 
     raw_cord_x = (translation[0] + (dim[0]*dim[2]) / 2) / (dim[0]*dim[2])
     raw_cord_y = (- translation[1] + (dim[1]*dim[2]) / 2) / (dim[1]*dim[2])
     raw_pose = np.array([raw_cord_x, raw_cord_y, rotation[3]])
 
-    np.save('tile_pose.npy', tile_pose)
-    np.save('raw_pose.npy', raw_pose)
+    send_floats_via_udp('0.0.0.0', 12345, [tile_cord_x, tile_cord_y, rotation[3]]) # TILE
+    send_floats_via_udp('0.0.0.0', 54321, [raw_cord_x, raw_cord_y, rotation[3]])   # RAW
+
+    np.save('/tile_pose.npy', tile_pose)
+    np.save('/raw_pose.npy', raw_pose)
