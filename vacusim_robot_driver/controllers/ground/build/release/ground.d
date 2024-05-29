b3b625f48252e63c483ad1123d295e1818bc1810
Mark Oliver Mints <mark@mints.de>
2024-05-29 15:55:48 +0200

    [Add] Apple Silicon (arm64) architecture build

--- vacusim_robot_driver/controllers/ground/build/release/ground.d
+++ vacusim_robot_driver/controllers/ground/build/release/ground.d
@@ -1,7 +1,7 @@
 build/release/ground.o: ground.c \
- /home/ecvb/.ros/webotsR2023b/webots/include/controller/c/webots/display.h \
- /home/ecvb/.ros/webotsR2023b/webots/include/controller/c/webots/types.h \
- /home/ecvb/.ros/webotsR2023b/webots/include/controller/c/webots/robot.h \
- /home/ecvb/.ros/webotsR2023b/webots/include/controller/c/webots/nodes.h \
- /home/ecvb/.ros/webotsR2023b/webots/include/controller/c/webots/supervisor.h \
- /home/ecvb/.ros/webotsR2023b/webots/include/controller/c/webots/contact_point.h
+  /Applications/Webots.app/Contents/include/controller/c/webots/display.h \
+  /Applications/Webots.app/Contents/include/controller/c/webots/types.h \
+  /Applications/Webots.app/Contents/include/controller/c/webots/robot.h \
+  /Applications/Webots.app/Contents/include/controller/c/webots/nodes.h \
+  /Applications/Webots.app/Contents/include/controller/c/webots/supervisor.h \
+  /Applications/Webots.app/Contents/include/controller/c/webots/contact_point.h
--- vacusim_robot_driver/controllers/large_ground/build/release/large_ground.d
+++ vacusim_robot_driver/controllers/large_ground/build/release/large_ground.d
@@ -1,7 +1,7 @@
 build/release/large_ground.o: large_ground.c \
- /home/ecvb/.ros/webotsR2023b/webots/include/controller/c/webots/display.h \
- /home/ecvb/.ros/webotsR2023b/webots/include/controller/c/webots/types.h \
- /home/ecvb/.ros/webotsR2023b/webots/include/controller/c/webots/robot.h \
- /home/ecvb/.ros/webotsR2023b/webots/include/controller/c/webots/nodes.h \
- /home/ecvb/.ros/webotsR2023b/webots/include/controller/c/webots/supervisor.h \
- /home/ecvb/.ros/webotsR2023b/webots/include/controller/c/webots/contact_point.h
+  /Applications/Webots.app/Contents/include/controller/c/webots/display.h \
+  /Applications/Webots.app/Contents/include/controller/c/webots/types.h \
+  /Applications/Webots.app/Contents/include/controller/c/webots/robot.h \
+  /Applications/Webots.app/Contents/include/controller/c/webots/nodes.h \
+  /Applications/Webots.app/Contents/include/controller/c/webots/supervisor.h \
+  /Applications/Webots.app/Contents/include/controller/c/webots/contact_point.h
